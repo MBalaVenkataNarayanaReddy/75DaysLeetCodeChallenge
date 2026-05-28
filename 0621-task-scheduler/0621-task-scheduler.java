0523646b51b21108cc8ff44252cfdebf8bd77639
M Bala Venkata Narayana Reddy <mbvnreddy@gmail.com>
2026-05-28 08:36:28 +0530

    Time: 2 ms (98.57%), Space: 48.4 MB (37.33%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        int[] frequency = new int[26];
+
+        for (char task : tasks) {
+            frequency[task - 'A']++;
+        }
+
+        int maxFrequency = 0;
+
+        for (int count : frequency) {
+            maxFrequency = Math.max(maxFrequency, count);
+        }
+
+        int maxCount = 0;
+
+        for (int count : frequency) {
+            if (count == maxFrequency) {
+                maxCount++;
+            }
+        }
+
+        int minimumTime =
+                (maxFrequency - 1) * (n + 1) + maxCount;
+
+        return Math.max(tasks.length, minimumTime);
+    }
+}
